7c8910783b6febfccbeadb6d5a2ec69d0e500734
Rafi Hakim <rhakim@ws67lx.lsv.uni-saarland.de>
2026-03-25 20:59:18 +0100

    prepare for deepseek

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ source /nethome/rhakim/projects/deepconfTesting/scripts/rename_gpus.sh
 
 # setup basic paths
 export PROJECT_DIR=/nethome/rhakim/projects/deepconfTesting
-export OUTPUT_DIR=/data/users/rhakim/logs/deepconfTesting
+#export OUTPUT_DIR=/data/users/rhakim/logs/deepconfTesting
 export CACHE_BASE_DIR=/data/users/zakhalili/models
 
 # cd to project dir
